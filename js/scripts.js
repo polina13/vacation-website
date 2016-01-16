ce073c7864ab3058b3c21f599667116b36d7706d
Polina Nenchev <polina.mihova@yahoo.com>
2016-01-15 16:03:38 -0800

    Final edit of vacation website files

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,45 @@
 $(document).ready(function() {
+
   $("#vacationForm").submit(function(event) {
     var gender = $("select#gender").val();
     var age = $("select#age").val();
     var attitude = $("select#attitude").val();
     var budget = $("select#budget").val();
-    var activities = $("select#activites").val();
+    var activities = $("select#activities").val();
 
-// var form = ["gender", "age", "attitude", "budget", "activites"].submit(function(event) {}
 
-    if (gender === 'female' && age === 'adult' && attitude === 'relax' && budget === 'under-$2,000' && activities === 'shopping') {
+    if (gender === 'male' && age === 'youth' && attitude === 'relax' && budget === 'above-$2,000' && activities === 'outdoor') {
       $("#destination2").show()
-      $("#destination2").hide()}
-
-   if (gender === 'female' && age === 'youth' && attitude !== 'party' && budget === 'above-$2,000' && activities === 'shopping') {
-      $("#destination1").show()
-      $("#destination1").hide()}
-
-   if (gender === 'male' && age !== 'adult' && attitude !== 'relax' && budget === 'under-$2,000' && activities === 'outdoor') {
-      $("#destination2").show()
-      $("#destination2").hide()}
-
-   if (gender === 'female' && age === 'youth' && attitude === 'party' && budget === 'above-$2,000' && activities === 'outdoor') {
+    } else {
+        $("#destination2").hide();
+    }
+    //
+    //
+    if (gender === 'male' && age === 'adult' && attitude === 'party' && budget === 'above-$2,000' && activities === 'outdoor') {
       $("#destination3").show()
-      $("#destination3").hide()}
-
-   if (gender === 'male' && age !== 'adult' && attitude === 'party' && budget === 'under-$2,000' && activities === 'outdoor') {
+    } else {
+        $("#destination3").hide();
+    }
+    //
+    if (gender === 'female' && age === 'adult' && attitude === 'both' && budget === 'above-$2,000' && activities === 'shopping') {
+      $("#destination3").show()
+    } else {
+        $("#destination3").hide();
+    }
+    //
+    if (gender === 'male' && age === 'adult' && attitude === 'both' && budget === 'under-$2,000' && activities === 'shopping') {
       $("#destination1").show()
-      $("#destination1").hide()}
+    } else {
+        $("#destination1").hide();
+    }
+    //
+    if (gender === 'female' && age === 'adult' && attitude === 'relax' && budget === 'under-$2,000' && activities === 'outdoor') {
+      $("#destination3").show()
+    } else {
+        $("#destination3").hide();
+    }
+    //
 
-   else {
-     $("#error").show();
-
-
-   }
 
     event.preventDefault();
 
